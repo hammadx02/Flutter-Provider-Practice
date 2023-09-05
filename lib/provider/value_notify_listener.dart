d391208bf5edeb40761f9117ab7f319a32b3fd9c
hammadx02 <hammadaali02@gmail.com>
2023-09-05 22:05:53 +0500

    updated

--- lib/provider/value_notify_listener.dart
+++ lib/provider/value_notify_listener.dart
@@ -4,31 +4,63 @@ class NotifyListnerScreen extends StatelessWidget {
   NotifyListnerScreen({super.key});
 
   ValueNotifier<int> _counter = ValueNotifier<int>(0);
-
+  ValueNotifier<bool> toggle = ValueNotifier<bool>(false);
   @override
   Widget build(BuildContext context) {
+    print('build');
     return Scaffold(
       appBar: AppBar(
         title: const Text(
           'Notify Listner',
         ),
       ),
-      body: Center(
-        child: ValueListenableBuilder(
-          valueListenable: _counter,
-          builder: (context, value, child) {
-            return Text(
-              _counter.value.toString(),
-              style: const TextStyle(
-                fontSize: 50,
-              ),
-            );
-          },
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          ValueListenableBuilder(
+            valueListenable: toggle,
+            builder: (context, value, child) {
+              return Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: TextFormField(
+                  obscureText: toggle.value,
+                  decoration: InputDecoration(
+                    hintText: 'Password',
+                    suffix: InkWell(
+                      onTap: () {
+                        toggle.value = !toggle.value;
+                      },
+                      child: Icon(
+                        toggle.value
+                            ? Icons.visibility_off_outlined
+                            : Icons.visibility_outlined,
+                      ),
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
+          Center(
+            child: ValueListenableBuilder(
+              valueListenable: _counter,
+              builder: (context, value, child) {
+                return Text(
+                  _counter.value.toString(),
+                  style: const TextStyle(
+                    fontSize: 50,
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           _counter.value++;
+          print(_counter.value.toString());
         },
         child: const Icon(
           Icons.add,
